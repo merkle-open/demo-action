2717634d5d6813d4a69adfe8fb3227adf4e127a8
Jonas Furrer <jonas.furrer@namics.com>
2020-01-29 08:48:01 +0100

    Update CI scripts

--- ci/output.sh
+++ ci/output.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 TAG=1.2.3.4.5
 echo "::set-output name=latest_tag::$TAG"
+echo "::set-env name=lastest_tag_env::$TAG"
--- ci/showoutput.sh
+++ ci/showoutput.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "LATEST TAG: $TAG"
+
+echo "LATEST TAG ENV: $latest_tag_env"
